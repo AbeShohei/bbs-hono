7adce62fce5f8890496c94839eb98c6c8ebf5791
shohei <shokoi0618@gmail.com>
2025-10-23 02:18:07 +0900

    Vercel向けのハンドラーを更新し、不要な依存関係を削除

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -1,7 +1,5 @@
 import app from '../src/app'
-import { handle } from '@hono/vercel'
 
 export const config = { runtime: 'edge' }
 
-export default handle(app)
-
+export default (req: Request) => app.fetch(req)
